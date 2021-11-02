f7a789e530bc32ff3a7edb31716d3e2e9150ecef
radyjjka <76512698+radyjjka@users.noreply.github.com>
2021-11-02 23:23:29 +0200

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,23 @@
+anychart.onDocumentReady(function () {
+    data = [];
+    dataSet = anychart.data.set(data);
+    let valueMapper = dataSet.mapAs({x: 0, value: 1, fill: 2, stroke: 2});
+    let chart = anychart.column();
+    chart.column(valueMapper);
+    chart.container("chart");
+    chart.draw();
+});
+let columnCounter = 1;
+function addRow() {
+    let stringReader = document.querySelector('.findInfo').value;
+    let unformattedString = stringReader.split(' ');
+    let array = [];
+    for (let i = 0; i < unformattedString.length; i++) {
+        array[i] = parseInt(unformattedString[i]);
+        let value = array[i];
+        let name = "Column #" + columnCounter;
+        columnCounter++;
+        let dataToChart = [name, value];
+        dataSet.append(dataToChart);
+    }
+}
